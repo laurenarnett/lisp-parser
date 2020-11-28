87ea669465023e0f2cc41ffdcfa4392d51d3bf3a
Lauren Arnett <lba2138@columbia.edu>
2020-11-28 16:49:15 -0500

    First Commit

---
+++ src/Main.hs
@@ -0,0 +1,48 @@
+module Main where
+
+import RIO
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import System.Console.Haskeline
+import RIO.Char (toLower)
+import qualified RIO.Text as T
+
+main :: IO ()
+main = runInputT defaultSettings loop 
+
+loop :: InputT IO ()
+loop = do
+  input <- getInputLine "LISP> "
+  case map toLower <$> input of
+    Nothing -> return ()
+    Just "quit" -> return ()
+    Just "" -> loop
+    Just input -> outputStrLn input >> loop
+
+type Parser = Parsec Void Text
+
+data Expr =
+    Symbol Text
+  | Number Double
+  | Exprs [Expr]
+  deriving (Show)
+
+sc :: Parser ()
+sc = L.space space1 (L.skipLineComment ";;") (L.skipBlockComment "/*" "*/")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+
+
+run :: Parser a -> Text -> Either (ParseErrorBundle Text Void) a
+run p input = runParser p "" input
+
+expr :: Parser Expr
+expr = do
+  symbolOrNum <- lexeme $ takeWhile1P Nothing (/= ' ')
+  pure $ case readMaybe (T.unpack symbolOrNum) of
+    Nothing -> Symbol symbolOrNum
+    Just n -> Number n
+  
